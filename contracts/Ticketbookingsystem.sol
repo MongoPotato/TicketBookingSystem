ec61e9696d868912d308235edc730d3302bd8a3e
"patrick" <>
2021-11-17 18:46:31 +0100

    added documentation

--- contracts/Ticketbookingsystem.sol
+++ contracts/Ticketbookingsystem.sol
@@ -63,8 +63,15 @@ contract Show is ERC721 {
     }
     */
     
+    
+    /**
+     * constructor that takes in the title, amountOfSeatpPerRow, the amount of rows, the date of the show, then the link for the seat
+     * It generates every seat available for that show.
+     * 
+    **/
+    
     constructor(string memory _title, uint _amountOfSeatpPerRow, uint _rows, string memory _date, string memory _linkSeatView) ERC721("Group 9 Ticket System", "G9TSys"){
-        title = _title;
+        title = _title; 
         amountOfSeatpPerRow = _amountOfSeatpPerRow;
         linkSeatView = _linkSeatView;
         owner = payable(msg.sender);
@@ -76,6 +83,13 @@ contract Show is ERC721 {
         }
     }
     
+    /**
+     * createTickets takes in the price of each ticket 
+     * and creates a ticket for every seat.
+     * It transfers ownership of every ticket to the organizer of the show
+     *  
+     * 
+    **/
     function createTickets() public payable {
         require(msg.sender == owner, 'Not owner to of Ticket system');
         ticketPrice = msg.value;
@@ -88,6 +102,13 @@ contract Show is ERC721 {
         }
     }
     
+    
+    /**
+     * buyTicket function that checks available tickets and you get a random ticket
+     *  that is in the amount of available free tickets.
+     * The sender transfers funds to the organizer of the show
+     * returns the TokenId
+    **/
     
     function buyTicket() public payable returns(uint256){
         uint counter = 0;
@@ -108,6 +129,12 @@ contract Show is ERC721 {
         return tickets[counter].tokenId;
     }
     
+    /**
+     * Returns the ticket you have bought at a show
+     * else returns -1
+     * 
+    **/
+    
     function getTicket() view public returns(int) {
         address customer = msg.sender;
         for(uint256 i = 0; i < tickets.length; i++){
@@ -117,6 +144,12 @@ contract Show is ERC721 {
         }
         return -1;
     }
+    
+    /**
+     * Returns the tokenId of the ticket you have bought at a show
+     * else returns -1
+     * 
+    **/
     
     function getTockenId() view public returns(int) {
         address customer = msg.sender;
@@ -128,45 +161,80 @@ contract Show is ERC721 {
         return -1;
     }
     
+    /**
+     * Takes in the tokenId and verifies if it the owner of the token ticket
+     * 
+    **/
+    
     function verifyOwner(uint256 tokenId) public view returns (address) {
         return tickets[tokenId].owner;
     }
     
+    
+    /**
+     * Function refundTickets requires that the organizer of the show 
+     * is the sender. Then it goes through the whole function and transfer
+     * ownership of the token to the organizer and transfer the value
+     * of the tickets back to each person that have bought a ticket.
+     * 
+     * 
+    **/
     function refundTickets() payable public{
         require(msg.sender == owner);
         for(uint i = 0; i < tickets.length; i++){
             if(tickets[i].sold == true){
                 _transfer(ownerOf(tickets[i].tokenId), owner, tickets[i].tokenId);
-                payable(ownerOf(tickets[i].tokenId)).transfer(ticketPrice);
+                payable(ownerOf(tickets[i].tokenId)).transfer(ticketPrice); //i think this transfer funds to the owner of the ticket ??
                 tickets[i].owner = owner;
                 tickets[i].sold = false;
             }
         }
     }
     
+    /**
+     * Function tradeticket takes in the address of the person you want to trade with and your tokenid
+     * you want to trade to that address. 
+     * 
+     * Requires that you have an Approval from the other person and that you own the token that
+     * you have in your input.
+     * 
+     * Then we transfer the token ticket to the toaddress and the sender receives the amount of 
+     * ether he used to buy his ether token ticket.
+     * 
+    **/
+    
     function tradeTicket(address toaddress, uint256 tokenid) payable public{
-        //check owner take in new address to trade with and transfer ownership then payback to the previous owner
         address customer = msg.sender;
-        for(uint256 i = 0; i < tickets.length; i++){
+        for(uint256 i = 0; i < tickets.length; i++){ //checks if you have a ticket
             if(customer == tickets[i].owner && tokenid == tickets[i].tokenId){
                 i = tickets.length;
             }
         }
-        require(customer == tickets[tokenid].owner || TicketApproval[tokenid] == customer);
+        require(customer == tickets[tokenid].owner && TicketApproval[tokenid] == customer); 
         _transfer(customer, toaddress, tickets[tokenid].tokenId);
-        payable(toaddress).transfer(ticketPrice);
+        payable(toaddress).transfer(ticketPrice); //i think this transfer funds to the owner of the ticket ??
         tickets[tokenid].owner = toaddress;
     }
     
+    /**
+     * Function Approves takes in the address of the person you want to trade the ticket with and get money. 
+     * If we take an example where C request the ticket of D. Then C sends this request and D approves the 
+     * request and accepts the trade with C. C obtains the token from D while D gains his ether back for the 
+     * price of the ticket he bought.
+     * 
+     * 
+    **/
+    
     function Approves(address toaddress, uint256 tokenid) external payable{
         address customer = msg.sender;
-        for(uint256 i = 0; i < tickets.length; i++){
+        for(uint256 i = 0; i < tickets.length; i++){ //checks if the sender has a ticket and if the token he sends in is valid
             if(customer == tickets[i].owner && tokenid == tickets[i].tokenId){
                 i = tickets.length;
             }
         }
-        require(toaddress == tickets[tokenid].owner && customer != toaddress);
-        TicketApproval[tokenid] = toaddress;
+        require(toaddress == tickets[tokenid].owner && customer != toaddress); //we require that the address of sender is 
+        //different from the address you want to send to and that the token you send in belongs to the address you send in
+        TicketApproval[tokenid] = toaddress; //we then send an approval
         emit Approval(customer, toaddress, tokenid);
     }
     
