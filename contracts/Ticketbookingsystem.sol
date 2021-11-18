9bb458680cf7eb0eaadf4cae047c6df0708118df
"patrick" <>
2021-11-18 01:40:31 +0100

    added methods in ticketbooking i did manage to make it work to call from ticketbooking to show

--- contracts/Ticketbookingsystem.sol
+++ contracts/Ticketbookingsystem.sol
@@ -33,6 +33,7 @@ contract Show is ERC721 {
     address payable owner;
     Seat private seat;
     uint256 ticketPrice = 1 ether;
+    uint Showid;
     
     mapping (uint => address) TicketApproval;
     
@@ -70,17 +71,27 @@ contract Show is ERC721 {
      * 
     **/
     
-    constructor(string memory _title, uint _amountOfSeatpPerRow, uint _rows, string memory _date, string memory _linkSeatView) ERC721("Group 9 Ticket System", "G9TSys"){
+    constructor(uint _Showid, string memory _title, uint _amountOfSeatpPerRow, uint _rows, string memory _date, string memory _linkSeatView) ERC721("Group 9 Ticket System", "G9TSys"){
         title = _title; 
         amountOfSeatpPerRow = _amountOfSeatpPerRow;
         linkSeatView = _linkSeatView;
         owner = payable(msg.sender);
+        Showid = _Showid;
         
         for(uint i = 0; i < _rows; i++){
             for(uint j = 0; j < amountOfSeatpPerRow; j++){
                 seats.push(Seat({title: title, date: _date, seatNumber: j, row: i, linkSeatView: linkSeatView}));
             }
         }
+    }
+    
+    
+    function getTitle() public view returns(string memory){
+        return title;
+    }
+    
+    function getShowId() public view returns(uint){
+        return Showid;
     }
     
     /**
@@ -246,20 +257,77 @@ contract TicketBooking {
     
     string private names;
     Show[] private shows;
+    uint Showid = 0;
     //list of shows and not just 1 show
     constructor(string memory _names){
         names = _names;
     }
     
-    function create(string memory _title, uint _amountOfSeatpPerRow, uint _rows, string memory _date, string memory _linkSeatView) public {
-        Show show = new Show(_title, _amountOfSeatpPerRow, _rows, _date, _linkSeatView);
+    //title is id
+    function create(string memory _title, uint _amountOfSeatpPerRow, uint _rows, string memory _date, string memory _linkSeatView) public returns(uint) {
+        Showid = Showid + 1;
+        Show show = new Show(Showid, _title, _amountOfSeatpPerRow, _rows, _date, _linkSeatView);
         show.createTickets();
         shows.push(show);
+        return Showid - 1;
     }
     
-    function getShow(uint index) public  view returns(Show show){
-        return shows[index];
+    function getShowTitleFromId(uint showid) public view returns(string memory){
+        for(uint i = 0; i < shows.length; i++){
+            if(shows[i].getShowId() == showid){
+                return shows[i].getTitle();
+            }
+        }
+        return "-1";
     }
- 
     
+    function verifyShowId(uint showid) private view returns(int){
+        for(uint i = 0; i < shows.length; i++){
+            if(shows[i].getShowId() == showid){
+                return int(shows[i].getShowId());
+            }
+        }
+        return -1;
+    }
+    
+    function check(uint showid) private view returns (uint){
+        int i = verifyShowId(showid);
+        require(i == -1, "Show does not exist wrong showID");
+        return uint(i);
+    }
+    
+    function createTickets(uint showid) public{
+        uint i = check(showid);
+        shows[i].createTickets();
+    }
+    
+    function buyTicket(uint showid) public returns(uint256){
+        uint i = check(showid);
+        return shows[i].buyTicket();
+    }
+    
+    function verifyOwner(uint showid, uint256 tokenid) public view returns(address){
+        uint i = check(showid);
+        return shows[i].verifyOwner(tokenid);
+    }
+    
+    function getTockenId(uint showid) public view returns(int){
+        uint i = check(showid);
+        return shows[i].getTockenId();
+    }
+    
+    function refundTickets(uint showid) public{
+        uint i = check(showid);
+        shows[i].refundTickets(); 
+    }
+    
+    function approveTradeTickets(uint showid, address toaddress, uint256 tokenid) public{
+        uint i = check(showid);
+        shows[i].Approves(toaddress, tokenid);
+    }
+    
+    function tradeTicketsWithApproval(uint showid, address toaddress, uint256 tokenid) public {
+        uint i = check(showid);
+        shows[i].tradeTicket(toaddress, tokenid);
+    }
 }
